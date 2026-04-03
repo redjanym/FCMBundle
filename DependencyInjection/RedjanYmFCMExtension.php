c3ec4ed6c00a0a52b5fb89dc41171452fde4bfb5
Progi1984 <progi1984@gmail.com>
2026-04-03 16:48:33 +0200

    Adapt signature  of RedjanYmFCMExtension for symfony/dependency-injection 8+ (#38)
    Co-authored-by: Progi1984 <franck@lefevre.dev>

--- DependencyInjection/RedjanYmFCMExtension.php
+++ DependencyInjection/RedjanYmFCMExtension.php
@@ -17,7 +17,7 @@ class RedjanYmFCMExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
